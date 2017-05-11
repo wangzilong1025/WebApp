8b4bec6785203926c0eed83edc0d6ae770113bb2
wangzilong1025 <15049129210@163.com>
2017-05-11 23:27:18 +0800

    UserLogin文件的更新2017-05-11

--- WebApp/src/main/java/com/sandi/web/model/UserLogin.java
+++ WebApp/src/main/java/com/sandi/web/model/UserLogin.java
@@ -26,46 +26,77 @@ public class UserLogin implements Serializable{
      * 用户状态(用户为2就是普通用户，用户为1就是管理员，用户为0就是超级管理员)
      */
     private int userStatus;
+    /**
+     * 用户安全设置的问题
+     */
+    private String safeQuestion;
+    /**
+     * 用户安全设置的答案
+     */
+    private String safeAnswer;
+
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
     public int getUserId() {
         return userId;
     }
+
     public void setUserId(int userId) {
         this.userId = userId;
     }
+
     public String getUserName() {
         return userName;
     }
+
     public void setUserName(String userName) {
         this.userName = userName;
     }
+
     public String getUserPassword() {
         return userPassword;
     }
+
     public void setUserPassword(String userPassword) {
         this.userPassword = userPassword;
     }
+
     public int getUserStatus() {
         return userStatus;
     }
+
     public void setUserStatus(int userStatus) {
         this.userStatus = userStatus;
     }
-    public UserLogin(int userId, String userName, String userPassword,
-                     int userStatus) {
-        super();
-        this.userId = userId;
-        this.userName = userName;
-        this.userPassword = userPassword;
-        this.userStatus = userStatus;
-    }
-    public UserLogin() {
-        super();
-    }
-    @Override
-    public String toString() {
-        return "UserLogin [userId=" + userId + ", userName=" + userName
-                + ", userPassword=" + userPassword + ", userStatus="
-                + userStatus + "]";
+
+    public String getSafeQuestion() {
+        return safeQuestion;
     }
 
+    public void setSafeQuestion(String safeQuestion) {
+        this.safeQuestion = safeQuestion;
+    }
+
+    public String getSafeAnswer() {
+        return safeAnswer;
+    }
+
+    public void setSafeAnswer(String safeAnswer) {
+        this.safeAnswer = safeAnswer;
+    }
+
+   /* @Override
+    public String toString() {
+        return "UserLogin{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", userStatus=" + userStatus +
+                ", safeQuestion='" + safeQuestion + '\'' +
+                ", safeAnswer='" + safeAnswer + '\'' +
+                '}';
+    }*/
 }
