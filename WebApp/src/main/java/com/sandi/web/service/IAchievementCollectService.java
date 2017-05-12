91c3c20e78757777cbf6bcd18ef62bdcb99b9362
wangzilong1025 <15049129210@163.com>
2017-05-12 22:19:53 +0800

    IAchievementCollectService遍历用户收藏的科研成果（IAchievementCollectService----Service层文件）-------------更新于2017-05-12

--- WebApp/src/main/java/com/sandi/web/service/IAchievementCollectService.java
+++ WebApp/src/main/java/com/sandi/web/service/IAchievementCollectService.java
@@ -1,7 +1,13 @@
 package com.sandi.web.service;
 
+import com.sandi.web.model.AchievementCollect;
+
+import java.util.List;
+
 /**
  * Created by 15049 on 2017-04-13.
  */
 public interface IAchievementCollectService {
+
+    public List<AchievementCollect> achievementCollectByAchievementId(int userinfoId);
 }
