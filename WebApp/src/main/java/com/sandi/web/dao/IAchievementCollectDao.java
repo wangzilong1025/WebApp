341cc33ed69d812098392ae68ce395e03e60a676
wangzilong1025 <15049129210@163.com>
2017-05-12 22:13:08 +0800

    增加遍历所有收藏的科研成果方法文件的方法-------------更新于2017-05-12

--- WebApp/src/main/java/com/sandi/web/dao/IAchievementCollectDao.java
+++ WebApp/src/main/java/com/sandi/web/dao/IAchievementCollectDao.java
@@ -1,7 +1,19 @@
 package com.sandi.web.dao;
 
+import com.sandi.web.model.AchievementCollect;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  * Created by 15049 on 2017-04-13.
  */
+@Repository("achievementCollectDao")
 public interface IAchievementCollectDao {
+    /**
+     * 用户的收藏科研成果遍历
+     * @param userinfoId
+     * @return
+     */
+    public List<AchievementCollect> achievementCollectByAchievementId(int userinfoId);
 }
