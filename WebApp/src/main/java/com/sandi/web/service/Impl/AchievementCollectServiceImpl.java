235a72be2468b2d7aca29e6acfa618d8ecbc7703
wangzilong1025 <15049129210@163.com>
2017-05-12 22:48:35 +0800

    收藏的科研成果的便利（AchievementCollectServiceImpl-----ServiceImpl层文件）-------------更新于2017-05-12

--- WebApp/src/main/java/com/sandi/web/service/Impl/AchievementCollectServiceImpl.java
+++ WebApp/src/main/java/com/sandi/web/service/Impl/AchievementCollectServiceImpl.java
@@ -1,7 +1,36 @@
 package com.sandi.web.service.Impl;
 
+import com.sandi.web.dao.IAchievementCollectDao;
+import com.sandi.web.model.AchievementCollect;
+import com.sandi.web.service.IAchievementCollectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by 15049 on 2017-04-13.
  */
-public class AchievementCollectServiceImpl {
+@Service("achievementCollectService")
+public class AchievementCollectServiceImpl implements IAchievementCollectService{
+
+    @Autowired
+    private IAchievementCollectDao achievementCollectDao;
+
+    public IAchievementCollectDao getAchievementCollectDao() {
+        return achievementCollectDao;
+    }
+    public void setAchievementCollectDao(IAchievementCollectDao achievementCollectDao) {
+        this.achievementCollectDao = achievementCollectDao;
+    }
+
+    /**
+     * 用户科研成果收藏遍历
+     * @param userinfoId
+     * @return
+     */
+    @Override
+    public List<AchievementCollect> achievementCollectByAchievementId(int userinfoId) {
+        return achievementCollectDao.achievementCollectByAchievementId(userinfoId);
+    }
 }
