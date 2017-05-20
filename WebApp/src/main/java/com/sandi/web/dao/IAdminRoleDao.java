37d405f95259eb33a7ef676dea63b2afe78f5309
wangzilong1025 <15049129210@163.com>
2017-05-21 00:54:26 +0800

    管理员角色Dao曾跟新方法2017-05-21

--- WebApp/src/main/java/com/sandi/web/dao/IAdminRoleDao.java
+++ WebApp/src/main/java/com/sandi/web/dao/IAdminRoleDao.java
@@ -1,7 +1,31 @@
 package com.sandi.web.dao;
 
+import com.sandi.web.model.AdminRole;
+import org.springframework.stereotype.Repository;
+
 /**
  * Created by 15049 on 2017-04-13.
  */
+@Repository("adminRoleDao")
 public interface IAdminRoleDao {
+    /**
+     * 管理员角色添加信息
+     * @param adminRole
+     * @return
+     */
+    public int addAdminRoleInfo(AdminRole adminRole);
+
+    /**
+     * 通过管理员的编号查询管理员的角色
+     * @param adminId
+     * @return
+     */
+    public AdminRole selectAdminRoleByAdminId(int adminId);
+
+    /**
+     * 管理员提交自己选择的角色
+     * @param adminRole
+     * @return
+     */
+    public int updateAdminRoleByAdminId(AdminRole adminRole);
 }
