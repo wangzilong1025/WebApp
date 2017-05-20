29bfab9c86cccb3e9417c41dab195b5ecfeaf205
wangzilong1025 <15049129210@163.com>
2017-05-21 00:50:53 +0800

    管理员角色方法更新，Service实现层2107-05-21

--- WebApp/src/main/java/com/sandi/web/service/Impl/AdminRoleServiceImpl.java
+++ WebApp/src/main/java/com/sandi/web/service/Impl/AdminRoleServiceImpl.java
@@ -1,9 +1,53 @@
 package com.sandi.web.service.Impl;
 
+import com.sandi.web.dao.IAdminRoleDao;
+import com.sandi.web.model.AdminRole;
 import com.sandi.web.service.IAdminRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
- * Created by 15049 on 2017-04-13.
+ * Created by 王子龙 on 2017-04-13.
  */
+@Service("adminRoleService")
 public class AdminRoleServiceImpl implements IAdminRoleService{
+    @Autowired
+    private IAdminRoleDao adminRoleDao;
+
+    public IAdminRoleDao getAdminRoleDao() {
+        return adminRoleDao;
+    }
+    public void setAdminRoleDao(IAdminRoleDao adminRoleDao) {
+        this.adminRoleDao = adminRoleDao;
+    }
+
+    /**
+     * 管理员角色的添加
+     * @param adminRole
+     * @return
+     */
+    @Override
+    public int addAdminRoleInfo(AdminRole adminRole) {
+        return adminRoleDao.addAdminRoleInfo(adminRole);
+    }
+
+    /**
+     * 根据管理员的ID，查询管理员的角色信息
+     * @param adminRole
+     * @return
+     */
+    @Override
+    public AdminRole selectAdminRoleByAdminId(int adminRole) {
+        return adminRoleDao.selectAdminRoleByAdminId(adminRole);
+    }
+
+    /**
+     * 管理员提交自己选择的角色
+     * @param adminRole
+     * @return
+     */
+    @Override
+    public int updateAdminRoleByAdminId(AdminRole adminRole) {
+        return adminRoleDao.updateAdminRoleByAdminId(adminRole);
+    }
 }
