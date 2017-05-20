7f00f64fb01b63580a64e60d38f61ea939018e84
wangzilong1025 <15049129210@163.com>
2017-05-21 00:49:56 +0800

    管理员角色Service接口层更新方法2017-05-21

--- WebApp/src/main/java/com/sandi/web/service/IAdminRoleService.java
+++ WebApp/src/main/java/com/sandi/web/service/IAdminRoleService.java
@@ -1,7 +1,29 @@
 package com.sandi.web.service;
 
+import com.sandi.web.model.AdminRole;
+
 /**
- * Created by 15049 on 2017-04-13.
+ * Created by 王子龙 on 2017-04-13.
  */
 public interface IAdminRoleService {
+    /**
+     * 管理员角色的添加
+     * @param adminRole
+     * @return
+     */
+    public int addAdminRoleInfo(AdminRole adminRole);
+
+    /**
+     * 根据管理员的ID查询管理员的角色
+     * @param adminRole
+     * @return
+     */
+    public AdminRole selectAdminRoleByAdminId(int adminRole);
+
+    /**
+     * 用户提交自交选择的管理员角色
+     * @param adminRole
+     * @return
+     */
+    public int updateAdminRoleByAdminId(AdminRole adminRole);
 }
