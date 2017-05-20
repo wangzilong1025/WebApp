1c9e95cd5271268e98a24ec6de554ea057cf1ec3
wangzilong1025 <15049129210@163.com>
2017-05-21 00:51:36 +0800

    管理员角色model层更新字段2017-05-21

--- WebApp/src/main/java/com/sandi/web/model/AdminRole.java
+++ WebApp/src/main/java/com/sandi/web/model/AdminRole.java
@@ -18,46 +18,60 @@ public class AdminRole {
      * 管理员角色备注
      */
     private String adminRoleNote;
+    /**
+     * 审批人是否同意
+     */
+    private int isNotApproval;
+
+
     public int getAdminRoleId() {
         return adminRoleId;
     }
+
     public void setAdminRoleId(int adminRoleId) {
         this.adminRoleId = adminRoleId;
     }
+
     public int getAdminId() {
         return adminId;
     }
+
     public void setAdminId(int adminId) {
         this.adminId = adminId;
     }
+
     public int getRoleId() {
         return roleId;
     }
+
     public void setRoleId(int roleId) {
         this.roleId = roleId;
     }
+
     public String getAdminRoleNote() {
         return adminRoleNote;
     }
+
     public void setAdminRoleNote(String adminRoleNote) {
         this.adminRoleNote = adminRoleNote;
     }
-    public AdminRole(int adminRoleId, int adminId, int roleId,
-                     String adminRoleNote) {
-        super();
-        this.adminRoleId = adminRoleId;
-        this.adminId = adminId;
-        this.roleId = roleId;
-        this.adminRoleNote = adminRoleNote;
-    }
-    public AdminRole() {
-        super();
-    }
-    @Override
-    public String toString() {
-        return "AdminRole [adminId=" + adminId + ", adminRoleId=" + adminRoleId
-                + ", adminRoleNote=" + adminRoleNote + ", roleId=" + roleId
-                + "]";
+
+    public int getIsNotApproval() {
+        return isNotApproval;
     }
 
+    public void setIsNotApproval(int isNotApproval) {
+        this.isNotApproval = isNotApproval;
+    }
+
+    @Override
+    public String toString() {
+        return "AdminRole{" +
+                "adminRoleId=" + adminRoleId +
+                ", adminId=" + adminId +
+                ", roleId=" + roleId +
+                ", adminRoleNote='" + adminRoleNote + '\'' +
+                ", isNotApproval=" + isNotApproval +
+                '}';
+    }
 }
