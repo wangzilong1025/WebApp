30751c65b06555c0ac700eb0599e9916a81d8f98
wangzilong1025 <15049129210@163.com>
2017-08-03 16:58:57 +0800

    2017年8月3日新项目添加到WEbApp

---
+++ province-mix/src/main/java/com/sandi/web/mix/StartUp.java
@@ -0,0 +1,10 @@
+package com.sandi.web.mix;
+
+/**
+ * Created by 15049 on 2017-06-30.
+ */
+public class StartUp {
+    public static void main(String[] args) {
+        com.alibaba.dubbo.container.Main.main(args);
+    }
+}
